e08a37e1754f068624d0db16f49665d2de632cae
Peter Richmond <dannydork10@gmail.com>
2015-08-31 16:55:55 -0700

    DB fix attempt #1.

--- server/db/database.js
+++ server/db/database.js
@@ -1,13 +1,21 @@
 var Sequelize = require('sequelize');
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : '',
-  database : 'halp'
-});
+// var connection = mysql.createConnection({
+//   host : 'localhost',
+//   user : 'root',
+//   password : '',
+//   database : 'halp'
+// });
 
-var sequelize = new Sequelize(process.env.MYSQL || 'mysql://root@localhost:3306/halp');
+// connection.connect(function(err){
+//   if(err){
+//   console.log('Error connecting to Db');
+//   return;
+//   }
+//   console.log('Connection established');
+// });
+
+var sequelize = new Sequelize(process.env.MYSQL || 'mysql://root@localhost:3306/heroku_91dcc327f5a9f3b');
 
 module.exports = sequelize;
