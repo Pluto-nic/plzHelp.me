0f046207f734ec438faa5a39701e30b1e28c946a
Peter Richmond <dannydork10@gmail.com>
2015-08-31 16:34:48 -0700

    Attempt to fix heroku connection.

--- server/db/database.js
+++ server/db/database.js
@@ -1,4 +1,12 @@
 var Sequelize = require('sequelize');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : '',
+  database : 'halp'
+});
 
 var sequelize = new Sequelize(process.env.MYSQL || 'mysql://root@localhost:3306/halp');
 
