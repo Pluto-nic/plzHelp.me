e165f855356f96d4c9cf3801745091ba09826034
Marq Short <marqshort@gmail.com>
2015-09-14 20:57:40 -0400

    Verification Code Logic

--- client/controllers/verificationController.js
+++ client/controllers/verificationController.js
@@ -1,11 +1,17 @@
 angular.module('app')
   .controller('verificationCtrl', ['$scope', '$state', 'appFact', '$http', function($scope, $state, appFact, $http){
+    var getCode = function(){
       $http.post('/getTwilioCode', {accountType: appFact.category, user_id: appFact.profile.user_id})
         .then(function(result){
           console.log(appFact);
           console.log('this is the result', result);
           $scope.code = result.data[0].verificationCode;
         });
+      if(!$scope.code){
+        setTimeout(getCode, 500);
+      }
+    }
+    getCode();
     $scope.backToProfile = function(){
       $state.go('index.list.overview'); 
     };   
